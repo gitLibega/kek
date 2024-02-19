b57ca12ca2453c2a270aec8334733815661262d9
Libega Ilya <libeeega@gmail.com>
2024-02-19 15:35:41 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,77 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <button nz-button [nzType]="'primary'" (click)="showModal()"><span>Show Modal</span></button>
+    <nz-modal [(nzVisible)]="isVisible" nzTitle="The first Modal" (nzOnCancel)="handleCancel()" (nzOnOk)="handleOk()">
+      <ng-container *nzModalContent>
+        <nz-select ngModel="lucy">
+      <nz-option nzValue="jack" nzLabel="Jack"></nz-option>
+      <nz-option nzValue="lucy" nzLabel="Lucy"></nz-option>
+      <nz-option nzValue="disabled" nzLabel="Disabled" nzDisabled></nz-option>
+    </nz-select>
+    
+<br\>
+
+<br\>
+<br\>
+<br\>
+<br\>
+<br\>
+<br\>
+<br\>
+<br\>
+<br\>
+<br\>
+<br\>
+<br\>
+<br\>
+<br\>
+<br\>
+<br\>
+<br\>
+<br\>
+<br\>
+<br\>
+<br\>
+<br\>
+<br\>
+<br\>
+<br\>
+<br\>
+<br\>
+<br\>
+<br\>
+<br\>
+<br\>
+<br\>
+<br\>
+<br\>
+<br\>
+<br\>
+<br\>
+<br\>
+      </ng-container>
+    </nz-modal>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'kek';
+  isVisible = false;
+
+  constructor() {}
+
+  showModal(): void {
+    this.isVisible = true;
+  }
+
+  handleOk(): void {
+    console.log('Button ok clicked!');
+    this.isVisible = false;
+  }
+
+  handleCancel(): void {
+    console.log('Button cancel clicked!');
+    this.isVisible = false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NzButtonModule } from 'ng-zorro-antd/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NzButtonModule,
+    NzSelectModule,
+    NzModalModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
